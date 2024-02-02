b7d634f09b6c70a24f159d13a45bc26a75769aff
Merlinkk <anshulkashyap2504@gmail.com>
2024-02-02 14:08:13 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Form from './components/Form2'
+import Form from './components/Form'
 
 function App() {
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,80 +1,49 @@
 import React, {useEffect, useState} from 'react';
 import { useForm } from 'react-hook-form';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function Form() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        reset,
-    } = useForm();
 
+function Form(){
+
+    const {register, formState : {errors}, handleSubmit} = useForm();
     const [submit, setSubmit] = useState(false);
+
 
     const onSubmit = (data) => {
         console.log('Form Submitted', data);
         setSubmit(true);
-        reset();
-    };
 
-    useEffect(() => {
-        console.log(errors)
-    },[errors])
+    }
+    
+    
 
-    return (
-        <>
+    return(
+        <div>
+            <h1>Registration Form</h1>
+
+            {submit ? <span id='success'>Registration successful!</span> : null}
+            <br /><br />
+
             <form onSubmit={handleSubmit(onSubmit)}>
-                <h1>Registration Form</h1>
-                {submit ? <span id='success'>Registration successful!</span> : null}
-                <br />
-                <br />
-                <input
-                    {...register('firstName', { required: true })}
-                    placeholder="First Name"
-                    type="text"
-                />
-                {errors.firstName && (
-                    <ErrorMessage message="First Name is required" />
-                )}
-                <br />
-                <br />
-                <input
-                    {...register('lastName', { required: true })}
-                    placeholder="Last Name"
-                    type="text"
-                />
-                {errors.lastName && (
-                    <ErrorMessage message="Last Name is required" />
-                )}
-                <br />
-                <br />
-                <input
-                    {...register('email', { required: true })}
-                    placeholder="Email"
-                    type="email"
-                />
-                {errors.email && <ErrorMessage message="Email is required" />}
-                <br />
-                <br />
-                <input
-                    {...register('phone', { required: true })}
-                    placeholder="Phone No:"
-                    type="number"
-                />
-                {errors.phone && (
-                    <ErrorMessage message="Phone No: is required" />
-                )}
-                <br />
-                <br />
-                <input id="submit" type="submit" value="Submit" />
-            </form>
-        </>
-    );
-}
+            <input type="text" placeholder='Enter Your First Name' {...register("firstName",{required:"Enter First Name"})}/><br/>
+        <p>{errors.firstName?.message}</p><br/>
 
-// A separate component for error messages
-function ErrorMessage({ message }) {
-    return <h3 style={{ color: 'red' }}>{message}</h3>;
+        <input type="text" placeholder='Enter Your Last Name' {...register("lastName",{required:"Enter Last Name"})}/><br/>
+        <p>{errors.lastName?.message}</p><br/>
+
+        <input type="email" placeholder='Enter Your Email' {...register("email",{required:"Enter Email  ", pattern:{value:/^\S+@\S+$/i, message:"Invalid email"}})}/><br/>
+        <p>{errors.email?.message}</p><br/>
+
+        <input type="password" placeholder='Enter Your Password' {...register("password", {required:"Password is required",
+        minLength:{value:4, message:"Password must be more than 4 characters"},
+        maxLength:{value:20, message:"Password cannot be more than 20 characters"}})}/><br/>
+        <p>{errors.password?.message}</p><br/>
+
+        <button id='submit'>Submit</button>
+      </form>
+    </div>
+    )
 }
 
 export default Form;
